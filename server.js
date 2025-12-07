99686e79def3f8e5ea9ab392fdec090660e61bfe
gelchangi <gelchangi@gmail.com>
2025-12-08 03:53:27 +0400

    Update default server port to 3000 in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { connectToDatabase, getDb, getClient } = require("./config/mongo");
 const lessonsRouter = require("./routes/lessons");
 const ordersRouter = require("./routes/orders");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
